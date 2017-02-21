cc8ed9df493995fe7bd3009ae04310b000fed8d5
Eliot Carlsen, Tom Yoder and Xing Li <xing@>
2017-02-20 16:56:27 -0800

    html, css, js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault()
+
+    var sentence = ($("#input").val()).split(" ");
+    console.log(sentence);
+    var newArray = sentence.forEach(function(word){
+      if word > characters (3).push(newArray){  
+      }else{
+
+      }
+    };
+
+
+
+
+  });
+});
